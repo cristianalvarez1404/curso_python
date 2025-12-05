746957a0641233c30eb2b04e8047eaa9ac4db2b6
cristian <cristianalvarez1496@gmail.com>
2025-12-05 18:53:08 -0500

    listas en python

---
+++ listas.py
@@ -0,0 +1,14 @@
+lista = [1,2,3,4,5,6,7,8,9,10]
+lista2 = ["hola","Mundo"]
+lista3 = [True,False]
+lista4 = [1,"hola",True]
+lista4[2] = False
+lista5 = [5,2,3,11,1]
+
+lista4.append("manzana")
+lista4.insert(1,"Pera")
+lista4.pop()
+lista4.remove("Pera")
+lista5.sort()
+print(lista5)
+
