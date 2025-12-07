2171b78207bcec978fe0bf8fe18eb5c457f90584
cristian <cristianalvarez1496@gmail.com>
2025-12-07 17:24:41 -0500

    methods and operation in lists

---
+++ 15.metodos_listas.py
@@ -0,0 +1,14 @@
+elementos = [True, 'carro',45,'moto']
+elementos1 = [False, 'libro',12]
+numeros = [5,14,20,2,7,6]
+
+# elementos.append('casa')
+# elementos.insert(1,'lapiz')
+# elementos.pop()
+# elementos.pop(1)
+# elementos.remove('carro')
+# elementos3 = elementos1.copy()
+# elementos.clear()
+# numeros.sort()
+
+#print(numeros)
