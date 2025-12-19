521b83a004f8f21b3e50ab57877f5cb3493220de
cristian <cristianalvarez1496@gmail.com>
2025-12-19 13:47:28 -0500

    tuplas

---
+++ tuplas.py
@@ -0,0 +1,11 @@
+
+objecto = ("Joe",False,42) #inmutabilidad - ordenadas
+# lista = ["Joe",False,42] #mutabilidad
+# objecto1 = ("Doe",)
+# print(type(objecto1))
+# lista[0] = "Doe"
+# print(objecto[2])
+var1, var2, var3 = objecto
+print(var1)
+print(var2)
+print(var3)
