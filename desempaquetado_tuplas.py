b3dcc98e0d50ccbc739df0ddb236285e8cc00a83
cristian <cristianalvarez1496@gmail.com>
2025-12-22 18:54:00 -0500

    desempaquetado tuplas

---
+++ desempaquetado_tuplas.py
@@ -0,0 +1,15 @@
+tupla = (1,2,3,4,5,6,7)
+var1, var2, var3, *var4 = tupla
+
+# lista = [(1,2),(3,4),(5,6)]
+
+# for num1, num2 in lista:
+#   print(num1,"-",num2)
+
+def coordernadas():
+  return (4,5)
+
+a,b = coordernadas()
+
+print(a)
+print(b)
