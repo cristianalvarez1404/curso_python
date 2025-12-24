96632b5ef6e15935309946f3b34713abc4971fb8
cristian <cristianalvarez1496@gmail.com>
2025-12-24 18:24:31 -0500

    methods tuplas

---
+++ 25.slicing_tuplas.py
@@ -0,0 +1,4 @@
+tupla = (1,5, True, [1,2,3],"Hola",4,5)
+
+var1 = tupla[-1::]
+print(var1)
---
+++ metodos_tuplas.py
@@ -0,0 +1,21 @@
+lista = (1,[4,"hola"])
+
+# lista[1][1] = False
+lista[0] = 100
+
+print(lista)
+# print(sum(lista))
+# print(len(lista))
+# print(sorted(lista))
+# print(min(lista))
+# print(max(lista))
+
+
+# print(lista.count(2))
+# print(lista.count(5))
+# print(lista.index(3))
+
+# if 41 in lista:
+#   print(lista.index(4))
+# else:
+#   print("no se encuentra el numero 4")
