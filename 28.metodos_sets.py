b3b82552d785b5ffdaf8e1c6d7fbf7713a4ee6db
cristian <cristianalvarez1496@gmail.com>
2025-12-28 13:45:52 -0500

    metodos sets

---
+++ 28.metodos_sets.py
@@ -0,0 +1,14 @@
+sets = {1,2,"Hello",False,True,2,1,False}
+
+# sets.add("CR 5")
+# sets.add(False)
+
+# sets.update({4,5,6,7,8,"Dg"})
+# sets.pop()
+# sets.clear()
+# sets.remove(5)
+# sets.discard(5)
+sets.update([1,2,4,5,6])
+
+
+print(sets)
