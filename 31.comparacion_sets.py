45104747874d324ecd64cd488506c4ba02a76468
cristian <cristianalvarez1496@gmail.com>
2025-12-31 17:50:11 -0500

    comparacion sets

---
+++ 31.comparacion_sets.py
@@ -0,0 +1,14 @@
+var1 = {"leer", "escribir"}
+var2 = {"leer","escribir"}
+
+resultado = var1 == var2
+resultado1 = var1 != var2
+
+resultado2 = var1 < var2
+resultado3 = var2 > var1
+
+resultado4 = var1 <= var2
+resultado5 = var2 >= var1
+
+print(resultado3)
+
