7c67e6c533301a5d3817ff92cde6f5cb426bff9a
cristian <cristianalvarez1496@gmail.com>
2026-01-23 13:51:00 -0500

    f strings

---
+++ 45.f_strings.py
@@ -0,0 +1,17 @@
+
+nombre = "Joe"
+edad = 45
+
+a = 10
+b = 20
+
+cifra = 1000000000
+decimal = 1000.78978974 
+
+# print("Hola " + nombre + " tienes " + str(edad) + " años.")
+print(f"Hola {nombre} tienes {str(edad)} años.")
+print(f"Sumatoria => {a + b}")
+print(f"Multiplicación => {a * b}")
+print(f"Total => {cifra:,}")
+print(f"Total => {cifra:,}")
+print(f"Redondear => {decimal:.3f}")
