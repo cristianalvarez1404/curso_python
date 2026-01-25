37569b57dba3f9fe636fd81d06d45a291046388a
cristian <cristianalvarez1496@gmail.com>
2026-01-24 20:38:33 -0500

    modulos python

---
+++ 46.modulos.py
@@ -0,0 +1,8 @@
+import operaciones
+import math
+from math import sqrt, acos, ceil, floor
+
+# print(operaciones.sumar(4,8))
+# print(operaciones.restar(4,8))
+# print(math.sqrt(25))
+print(sqrt(25))
