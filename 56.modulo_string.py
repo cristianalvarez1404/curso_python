6525ba1edf44d2058097ad4e297593c8f1078747
cristian <cristianalvarez1496@gmail.com>
2026-02-13 15:55:16 -0500

    modulo string

---
+++ 56.modulo_string.py
@@ -0,0 +1,25 @@
+import string
+import random
+
+# print(string.ascii_letters)
+# print(string.ascii_lowercase)
+# print(string.ascii_uppercase)
+# print(string.digits)
+# print(string.punctuation)
+
+# password = "123"
+# valid = False
+
+# for i in password:
+#   if i in string.ascii_letters:
+#     valid = True
+
+# print(valid)
+
+caracteres = string.ascii_letters + string.digits + string.punctuation
+
+# print(random.choice(caracteres))
+
+codigo = "".join(random.choice(caracteres) for _ in range(10))
+
+print(codigo)
