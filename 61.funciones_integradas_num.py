59acafa7f28ec4a1f940fdcdfd97c9213a6c3b66
cristian <cristianalvarez1496@gmail.com>
2026-02-24 15:21:07 -0500

    funciones integradas numeros

---
+++ 61.funciones_integradas_num.py
@@ -0,0 +1,29 @@
+
+num1 = -10
+num2 = 3.4
+
+print(type(num1))
+print(type(num2))
+
+print(abs(num1))
+print(divmod(10,3))
+
+lista = [10,20,30,40]
+print(sum(lista))
+print(max(lista))
+print(min(lista))
+
+print(bin(10))
+print(oct(10))
+print(hex(10))
+
+print(round(3.1416))
+print(round(3.5))
+
+num1 = 10.5
+num2 = int(num1)
+
+num3 = 15
+num4 = float(num3)
+
+print(num4)
