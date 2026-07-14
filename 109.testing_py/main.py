24abe8dd9139635c56bf5c23bf93c80cddb4ab48
cristiann <cristianandres1496@gmail.com>
2026-07-14 16:50:38 -0500

    testing- unittest

---
+++ 109.testing_py/main.py
@@ -0,0 +1,17 @@
+
+def sumar(a, b):
+  return a + b
+
+def restar(a, b):
+  return a - b
+
+def multiplicar(a, b):
+  return a * b
+
+def dividir(a, b):
+  return a / b
+
+# resultado = suma(5,5)
+
+# print(resultado == 10)
+# assert suma(6,6) == 13
---
+++ 109.testing_py/test_calculador.py
@@ -0,0 +1,26 @@
+import unittest
+from main import sumar, restar, multiplicar, dividir
+
+class TestCalculadora(unittest.TestCase):
+  def test_sumar(self):
+    resultado = sumar(5,5)
+    self.assertEqual(resultado, 10)
+  
+  def test_resta(self):
+    resultado = restar(6,2)
+    self.assertEqual(resultado, 4)
+  
+  def test_multiplicacion(self):
+    resultado = multiplicar(2,2)
+    self.assertEqual(resultado, 4)
+
+  def test_division(self):
+    resultado = dividir(6,2)
+    self.assertEqual(resultado,3)
+
+  def metodo_aux(self):
+    pass
+
+
+if __name__ == "__main__":
+  unittest.main()
